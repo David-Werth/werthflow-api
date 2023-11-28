d7ede52d3b5d6e515b2b70c6b1526900063b97fa
David Werth <info@davidwerth.com>
2023-11-28 06:54:34 +0100

    chore: changes to update moved tasks

--- src/controllers/folder.controller.ts
+++ src/controllers/folder.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { db } from '../utils/db';
+import { Task } from '@prisma/client';
 
 const folderClient = db.folder;
 
@@ -47,15 +48,44 @@ export async function updateFolder(req: Request, res: Response) {
 		const folderId = req.params.id;
 		const folderData = req.body;
 
-		const folder = await folderClient.update({
-			where: {
-				id: folderId,
-			},
-			data: {
-				title: folderData.folderTitle,
-			},
-		});
-		res.status(200).json({ message: 'Folder title updated: ', data: folder });
+		if (folderData.folderTitle) {
+			const folder = await folderClient.update({
+				where: {
+					id: folderId,
+				},
+				data: {
+					title: folderData.folderTitle,
+				},
+			});
+			res.status(200).json({ message: 'Folder title updated: ', data: folder });
+		} else {
+			const cleanedTasks: Task[] = folderData.map((t: Task) => {
+				return {
+					id: t.id,
+					index: t.index,
+					title: t.title,
+					content: t.content,
+					userId: t.userId,
+					sortableId: t.sortableId,
+				};
+			});
+
+			const folder = await folderClient.update({
+				where: {
+					id: folderId,
+				},
+				data: {
+					tasks: {
+						deleteMany: {},
+						createMany: {
+							data: cleanedTasks,
+						},
+					},
+				},
+			});
+
+			res.status(200).json({ message: 'Folder tasks updated: ', data: folder });
+		}
 	} catch (error: any) {
 		console.log(error);
 		res.status(404).json({ message: 'Folder not found: ', error: error });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,13 +3,11 @@ import {
 	createUser,
 	deleteUser,
 	getUserById,
-	updateUser,
 } from '../controllers/user.controller';
 
 const userRouter = Router();
 
 userRouter.post('/:id', createUser);
-userRouter.put('/:id', updateUser);
 userRouter.get('/:id', getUserById);
 userRouter.delete('/:id', deleteUser);
 
